dab242863d2db0a3b3ed91d9664cf5b11faee066
fgonzalezy <fgonzalezy@89ec0f24-a736-fa18-aac7-692ca77dc9f4>
2010-11-11 20:48:13 +0000

    Imprimiendo Matriz Pre y Post en JTextArea

--- trunk/RDP/src/control/AreaConsola.java
+++ trunk/RDP/src/control/AreaConsola.java
@@ -8,7 +8,6 @@ public class AreaConsola extends javax.swing.JScrollPane{
 
     RDP redpetri;
     JTextArea jta = new JTextArea(25,25);
-    private int click=-1;
 
     public AreaConsola(RDP r){
         this.redpetri=r;
@@ -17,9 +16,25 @@ public class AreaConsola extends javax.swing.JScrollPane{
     }
 
     public void escribeMatriz(){
+        int i,j;
         jta.setText("");
-        jta.append("Hola Presionando Boton Matriz\n");
+        jta.append("Matriz Pre\n");
         this.redpetri.generarMatrizPre();
+        this.redpetri.generarMatrizPost();
+        for(i=0;i<this.redpetri.numPlazas();i++){
+            for(j=0;j<this.redpetri.numTransiciones();j++){
+                jta.append("["+Integer.toString(this.redpetri.retornaPre(i,j))+"]");
+            }
+            jta.append("\n");
+        }
+
+        jta.append("\nMatriz Post\n");
+        for(i=0;i<this.redpetri.numPlazas();i++){
+            for(j=0;j<this.redpetri.numTransiciones();j++){
+                jta.append("["+Integer.toString(this.redpetri.retornaPost(i,j))+"]");
+            }
+            jta.append("\n");
+        }
     }
 
 }
