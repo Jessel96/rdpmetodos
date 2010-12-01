4470bffa98468fe4fb651f40415fe2c9c75265fd
fgonzalezy <fgonzalezy@89ec0f24-a736-fa18-aac7-692ca77dc9f4>
2010-12-01 17:29:01 +0000

    terrminado pero con falla

--- trunk/TalcaTour/src/Ejecutora/Frame.java
+++ trunk/TalcaTour/src/Ejecutora/Frame.java
@@ -4,20 +4,44 @@ package Ejecutora;
  * @author marza
  */
 import java.awt.*;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Frame extends JFrame{
 
     public AreaMapa am = new AreaMapa();
-
-
+    JButton recorrido = new JButton("Trazar Ruta");
+    JButton limpiar = new JButton("Limpiar Mapa");
 
     public Frame(){
 
         getContentPane().setLayout(new FlowLayout());
 
+        recorrido.setFocusable(false);
+        recorrido.setName("Trazar Ruta");
+        recorrido.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        recorrido.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        recorrido.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                am.setflag(1);
+                repaint();
+            }
+        });
+        limpiar.setFocusable(false);
+        limpiar.setName("Trazar Ruta");
+        limpiar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        limpiar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        limpiar.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                am.setflag(0);
+                repaint();
+            }
+        });
+
         add(am);
+        add(recorrido);
+        add(limpiar);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 	setTitle("Talca Tour");
